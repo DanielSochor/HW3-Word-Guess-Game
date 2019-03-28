f897d18fcbd3a67845361f790111b1945145be26
Daniel Sochor <47047939+DanielSochor@users.noreply.github.com>
2019-03-28 14:27:06 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,83 @@
 
+var gameInProgress = false;
+var dashedWord = "";
+var newDashedWord = "";
+var numberOfGuess = 0;
+var wordToGuess = "";
+var alreadyGuessedLetters = "";
+
 document.onkeypress = function (event) {
     var userEntry = event.key;
-    console.log(userEntry);
-
-    if (userEntry = " ") {
-        console.log("space");
-        document.getElementById("blink").style.display = "none";
-        game();
+    if ((userEntry == " ") && (gameInProgress == false)) {
+        start();
+    } else if (gameInProgress == true) {
+        gamePlay(userEntry, wordToGuess);
     }
 }
 
-function game() {
-    var neighborhoods = ["Lincoln Park", "Old Town", "River North"]
-    var choosen = neighborhoods[Math.floor(Math.random() * neighborhoods.length)];
-    console.log(choosen)
-
-    addDashes(choosen);
+function start() {
+    console.log("start");
+    document.getElementById("blink").style.display = "none";
+    document.getElementById("wordToGuessTitle").innerHTML = "Word To Guess:";
+    game();
+    gameInProgress = true;
 }
 
-var dashedWord = "";
+function game() {
+    //var neighborhoods = ["Lincoln Park", "Old Town", "River North"]
+    var neighborhoods = ["lincoln park", "old town", "river north"]
+    wordToGuess = neighborhoods[Math.floor(Math.random() * neighborhoods.length)];
+    console.log(wordToGuess)
+    addDashes(wordToGuess);
+    document.getElementById("guessedLettersTitle").innerHTML = "Guessed Letters:";
+}
 
-function addDashes(choosen) {
-    for (i = 0; i < choosen.length; i++) {
-        if (choosen.charAt(i) == " ") {
+function addDashes(wordToGuess) {
+    for (i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess.charAt(i) == " ") {
             dashedWord += " "
         } else {
             dashedWord += "-"
         }
+        newDashedWord=dashedWord;
     }
     document.getElementById("wordToGuess").innerHTML = dashedWord;
 }
+
+function gamePlay(userEntry, wordToGuess) {
+    if (!alreadyGuessedLetters.includes(userEntry)) {
+        numberOfGuess++;
+        alreadyGuessedLetters += (" " + userEntry);
+        document.getElementById("guessedLetters").innerHTML = alreadyGuessedLetters;
+        console.log("number of guesses:" + numberOfGuess);
+        if (wordToGuess.includes(userEntry)) {
+            console.log("word includes letter")
+            for (i = 0; i < wordToGuess.length; i++) {
+                //newDashedWord = dashedWord
+                if (wordToGuess.charAt(i) == userEntry) {
+                    console.log("position of letter:" + i);
+                    subString1 = newDashedWord.substring(0,i);
+                    subString2 = newDashedWord.substring(i+1,dashedWord.length);
+                    console.log(subString1);
+                    console.log(subString2);
+                    newDashedWord = subString1 + userEntry + subString2;
+                    console.log(newDashedWord);
+                    // newDashedWord = dashedWord.replace(dashedWord[i], userEntry);
+                    //console.log(newDashedWord[i]); 
+                    document.getElementById("wordToGuess").innerHTML = newDashedWord;
+                }
+            }
+        } else {
+            console.log("word dosen't include letter")
+        }
+    }
+}
+
+// function replaceStringCharacter (wordToGuess,userEntry){
+// part1 = wordToGuess.substring()
+// }
+
+//TODO:
+//Repeat Game
+//Wins/Losses
+//Alert you win
