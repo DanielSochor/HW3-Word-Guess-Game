2476cd598a18dc8905b05a61942bc30fe5275468
Daniel Sochor <47047939+DanielSochor@users.noreply.github.com>
2019-03-29 20:00:10 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,76 +2,114 @@
 var gameInProgress = false;
 var dashedWord = "";
 var newDashedWord = "";
-var numberOfGuess = 0;
-var wordToGuess = "";
+var numberOfGuesses = 0;
+var maximumNumberOfGuesses = 0;
+var phraseToGuess = "";
 var alreadyGuessedLetters = "";
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var gamesWon = 0;
+var gamesLost = 0;
+//var phrases = ["Lincoln Park", "Old Town", "River North"];
+var phrases = ["Have it your way","See if I care","Not everyone is a winner","Mistakes happen"]
+document.getElementById("newGame").style.visibility = "hidden";
+var newPuzzle = new Audio("assets/sounds/Puzzle Reveal.mp3");
+var solvePuzzle = new Audio("assets/sounds/Puzzle Solve.mp3");
+var failPuzzle = new Audio("assets/sounds/Puzzle Fail.mp3");
 
 document.onkeydown = function (event) {
     var userEntry = event.key;
     if ((userEntry == " ") && (gameInProgress == false)) {
-        start();
+        document.getElementById("blink").style.display = "none";
+        initializeGame();
     } else if (gameInProgress == true) {
-        gamePlay(userEntry, wordToGuess);
+        gamePlay(userEntry, phraseToGuess);
     }
 }
 
-function start() {
-    console.log("start");
-    document.getElementById("blink").style.display = "none";
-    document.getElementById("wordToGuessTitle").innerHTML = "Word To Guess:";
-    game();
+function initializeGame() {
     gameInProgress = true;
-}
-
-function game() {
-    //var neighborhoods = ["Lincoln Park", "Old Town", "River North"]
-    var neighborhoods = ["Lincoln Park", "Old Town", "River North"]
-    wordToGuess = neighborhoods[Math.floor(Math.random() * neighborhoods.length)];
-    console.log(wordToGuess)
-    addDashes(wordToGuess);
+    document.getElementById("blink").style.display = "none";
+    document.getElementById("phraseToGuessTitle").innerHTML = "Phrase To Guess:";
     document.getElementById("guessedLettersTitle").innerHTML = "Guessed Letters:";
+    document.getElementById("gamesWonTitle").innerHTML = "Games Won: ";
+    document.getElementById("gamesLostTitle").innerHTML = "Games Lost: ";
+    document.getElementById("gamesWon").innerHTML = gamesWon;
+    document.getElementById("gamesLost").innerHTML = gamesLost;
+    document.getElementById("newGame").style.visibility = "hidden";
+    gameStart();
 }
 
-function addDashes(wordToGuess) {
-    for (i = 0; i < wordToGuess.length; i++) {
-        if (wordToGuess.charAt(i) == " ") {
+function gameStart() {
+    newPuzzle.play();
+    document.getElementById("guessedLetters").innerHTML = "";
+    phraseToGuess = "";
+    alreadyGuessedLetters = "";
+    dashedWord = "";
+    numberOfGuesses = 0;
+    console.log("Start new game!");
+    phraseSelector();
+    gamePlay();
+}
+
+function phraseSelector() {
+    phraseToGuess = phrases[Math.floor(Math.random() * phrases.length)];
+    console.log(phraseToGuess)
+    addDashes(phraseToGuess);
+}
+
+function addDashes(phraseToGuess) {
+    for (i = 0; i < phraseToGuess.length; i++) {
+        if (phraseToGuess.charAt(i) == " ") {
             dashedWord += " "
         } else {
             dashedWord += "-"
         }
-        newDashedWord=dashedWord;
     }
-    document.getElementById("wordToGuess").innerHTML = dashedWord;
+    newDashedWord = dashedWord;
+    document.getElementById("phraseToGuess").innerHTML = dashedWord;
+    maximumNumberOfGuesses = Math.max(10, (phraseToGuess.length)*0.5);
+    console.log(maximumNumberOfGuesses);
 }
 
-function gamePlay(userEntry, wordToGuess) {
-    if (!alreadyGuessedLetters.includes(userEntry)) {
-        numberOfGuess++;
+function gamePlay(userEntry, phraseToGuess) {
+    if ((!alreadyGuessedLetters.includes(userEntry)) && (newDashedWord != phraseToGuess) && (alphabet.includes(userEntry))) {
+        numberOfGuesses ++;
         alreadyGuessedLetters += (" " + userEntry);
         document.getElementById("guessedLetters").innerHTML = alreadyGuessedLetters;
-        wordToGuessAllLowerCase = wordToGuess.toLowerCase();
-        if (wordToGuessAllLowerCase.includes(userEntry)) {
-            for (i = 0; i < wordToGuess.length; i++) {
-                if (wordToGuess.charAt(i) == userEntry.toLowerCase() || wordToGuess.charAt(i) == userEntry.toUpperCase()) {
-                    subString1 = newDashedWord.substring(0,i);
-                    subString2 = newDashedWord.substring(i+1,wordToGuess.length);
-                    fillLetter = wordToGuess.charAt(i);
+        phraseToGuessAllLowerCase = phraseToGuess.toLowerCase();
+        if (phraseToGuessAllLowerCase.includes(userEntry)) {
+            for (i = 0; i < phraseToGuess.length; i++) {
+                if ((phraseToGuess.charAt(i) == userEntry.toLowerCase()) || (phraseToGuess.charAt(i) == userEntry.toUpperCase())) {
+                    subString1 = newDashedWord.substring(0, i);
+                    subString2 = newDashedWord.substring(i + 1, phraseToGuess.length);
+                    fillLetter = phraseToGuess.charAt(i);
                     newDashedWord = subString1 + fillLetter + subString2;
-                    document.getElementById("wordToGuess").innerHTML = newDashedWord;
+                    document.getElementById("phraseToGuess").innerHTML = newDashedWord;
                 }
-            }
-            if(newDashedWord==wordToGuess){
-                alert("You win!")
             }
         } else {
             console.log("word dosen't include letter")
         }
     }
+    if ((newDashedWord == phraseToGuess) && (userEntry != " ")) {
+        solvePuzzle.play();
+        alert("You Win!")
+        gamesWon ++;
+        document.getElementById("gamesWon").innerHTML = gamesWon;
+        document.getElementById("newGame").style.visibility = "visible";
+    }
+    if ((numberOfGuesses == maximumNumberOfGuesses) && (userEntry != " ")){
+        failPuzzle.play();
+        alert("You Fail")
+        gamesLost ++;
+        document.getElementById("gamesLost").innerHTML = gamesLost;
+        document.getElementById("newGame").style.visibility = "visible";
+    }
+
 }
 
 //TODO:
-//shoe guessed word with proper case
-//Repeat Game
-//Wins/Losses
-//Alert you win in correct position
-//add button to restart game
+//fix font
+//more phrases
+//show answer
+//adjust button
